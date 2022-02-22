0ea767b045d0a9a89f12095bd3d9845167bd550c
child_duckling <19170969+quacksire@users.noreply.github.com>
2022-02-21 19:33:07 -0800

    remote qr adjusted

--- remote/pair-remote.js
+++ remote/pair-remote.js
@@ -1,15 +1,17 @@
 const QueryString = window.location.search;
 const urlParams = new URLSearchParams(QueryString);
-if (!urlParams.has('url')) document.location.href = '/remote'
+//if (!urlParams.has('url')) document.location.href = '/remote'
 let url = decodeURI(atob(urlParams.get('url')))
 
 document.getElementById('qr-code-link').href = url;
-document.getElementById('qr-code').src = "https://api.qrserver.com/v1/create-qr-code/?size=100x100&data=" + url + '&bgcolor=255-33-86&color=0-0-0&margin=10';
+document.getElementById('qr-code').src = "https://api.qrserver.com/v1/create-qr-code/?size=140x140&data=" + url + '&bgcolor=255-33-86&color=0-0-0&margin=10';
 document.getElementById('button').addEventListener('click', function () {
     //Clipboard.writeText(url);
     Clipboard.writeText = url
     document.getElementById('button').innerHTML = "Copied!";
 });
+
+if (isElectron()) document.getElementById('header').remove();
 
 
 // I know, this is cancer
