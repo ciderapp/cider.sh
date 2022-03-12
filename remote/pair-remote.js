20530d1661b3cc537ed5b79ea0fbdf452603e4b9
Core <64542347+coredev-uk@users.noreply.github.com>
2022-03-12 07:00:44 +0000

    and electron is sorted

--- remote/pair-remote.js
+++ remote/pair-remote.js
@@ -1,29 +1,19 @@
 const QueryString = window.location.search;
 const urlParams = new URLSearchParams(QueryString);
 if (!urlParams.has('url')) document.location.href = '/remote'
-let url = decodeURI(atob(urlParams.get('url')))
+const url = decodeURI(atob(urlParams.get('url')))
 
-document.getElementById('qr-code-link').href = url;
 document.getElementById('qr-code').src = "https://api.qrserver.com/v1/create-qr-code/?size=140x140&data=" + url + '&bgcolor=255-33-86&color=0-0-0&margin=10';
 document.getElementById('button').addEventListener('click', function () {
-    //Clipboard.writeText(url);
-    navigator.clipboard.writeText(url) // this is a website not electron backend
-    // const clip = new Clipboard() 
-    // clip.writeText(url)
+    navigator.clipboard.writeText(url)
     document.getElementById('button').innerHTML = "Copied!";
 });
 
-
 if (navigator.userAgent.toLowerCase().indexOf(' electron/') > -1) {
     document.getElementById('header').remove();
+    document.getElementById('qr-code-link').addEventListener('click', () => {
+        window.open(url) // its as simple as this :)
+    })
+} else {
+    document.getElementById('qr-code-link').href = url
 }
-
-
-// I know, this is cancer
-//if (isElectron()) { // Detect when running inapp so that we can open in browser instead -q
-//    const shell = require('electron').shell
-//    document.getElementById('url').addEventListener('click', () => {
-//        shell.openExternal(url)
-//    })
-//    document.getElementById('url').href = '#'
-//} else {
