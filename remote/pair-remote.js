5a23653d5370366ad086933d987ce1c4a84e3578
yazninja <71800112+yazninja@users.noreply.github.com>
2022-03-22 13:46:01 +0800

    Add Light Mode Support in pair remote screen (#8)

--- remote/pair-remote.js
+++ remote/pair-remote.js
@@ -2,8 +2,14 @@ const QueryString = window.location.search;
 const urlParams = new URLSearchParams(QueryString);
 if (!urlParams.has('url')) document.location.href = '/remote'
 const url = decodeURI(atob(urlParams.get('url')))
+if(window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches){
+    document.getElementById('qr-code').src = "https://api.qrserver.com/v1/create-qr-code/?size=140x140&data=" + url + '&bgcolor=250-35-59&color=235-235-235&margin=10';
 
-document.getElementById('qr-code').src = "https://api.qrserver.com/v1/create-qr-code/?size=140x140&data=" + url + '&bgcolor=255-33-86&color=0-0-0&margin=10';
+}
+else{
+    document.getElementById('qr-code').src = "https://api.qrserver.com/v1/create-qr-code/?size=140x140&data=" + url + '&bgcolor=255-33-86&color=0-0-0&margin=10';
+
+}
 document.getElementById('button').addEventListener('click', function () {
     navigator.clipboard.writeText(url)
     document.getElementById('button').innerHTML = "Copied!";
