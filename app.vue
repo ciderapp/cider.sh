8f81931cfd0b56bcc12c9868a0e6616c550e8d5d
Swiftzerr <elliotjarnit@gmail.com>
2022-11-09 15:51:41 -0800

    routiing

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
+  <div class="page">
     <NuxtPage />
   </div>
 </template>
+
+<style lang="scss">
+$primary-color: #161616;
+$text-color: #DCDCDC;
+
+body {
+  background-color: $primary-color;
+  margin: 0;
+}
+
+.page {
+  color: $text-color;
+}
+
+
+</style>
---
+++ pages/cider/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="flex center column">
+            <img class="logo" src="~/assets/cider.png" />
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+
+.logo {
+    width: 150px;
+    height: 150px;
+}
+
+.flex {
+    display: flex;
+}
+
+.center {
+    justify-content: center;
+    align-items: center;
+}
+
+.column {
+    flex-direction: column;
+}
+</style>
