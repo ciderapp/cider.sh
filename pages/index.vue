c3285a4d2ae3b91e31c1ba6c79e85152878b3b05
Swiftzerr <elliotjarnit@gmail.com>
2022-11-30 19:56:08 -0800

    more embed

--- pages/index.vue
+++ pages/index.vue
@@ -7,3 +7,13 @@
     </div>
   </div>
 </template>
+
+<script setup>
+useHead({
+  meta: {
+    property: 'og:title', content: 'Cider Collective',
+    property: 'og:description', content: 'A development collective working to make your life and user experience more flavorful starting with a glass of Cider.',
+    property: 'og:image', content: 'https://cider.sh/cider.png',
+  },
+})
+</script>
