4334793f05258e07ed42109536bc0d8f9980c804
yazninja <yazninja@cider.sh>
2024-05-04 02:09:52 +0300

    fix remote link

--- components/MobileNav.vue
+++ components/MobileNav.vue
@@ -18,10 +18,10 @@
             <UiSeparator class="my-4" />
           </div>
           <div class="links my-6 flex flex-col gap-y-5 font-bold">
-            <NuxtLink to="/remote" class="hover:text-primary">
+            <NuxtLink to="/downloads/remote" class="hover:text-primary">
               <Icon name="mdi:remote" class="mr-3" /> Remote
             </NuxtLink>
-            <NuxtLink to="/downloads">
+            <NuxtLink to="/downloads/client">
               <Icon name="mdi:downloads" class="mr-3" /> Download
             </NuxtLink>
             <NuxtLink to="/docs"> <Icon name="mdi:book-open" class="mr-3" /> Docs </NuxtLink>
@@ -31,7 +31,7 @@
           </div>
           <UiSeparator class="mt-4" />
           <p class="mb-4 mt-6 text-2xl font-bold">Site Pages</p>
-          <DocsNavlink :links="navigation" />
+          <DocsNavlink :links="navigation ?? []" />
         </UiScrollArea>
       </template>
     </UiSheetContent>
