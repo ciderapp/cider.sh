6b827a284a1a14a2f728c458995bf2b8e8f0ff6e
yazninja <71800112+yazninja@users.noreply.github.com>
2024-05-15 11:21:32 +0300

    Create [id].get.ts

---
+++ server/api/v2/play/[type]/[id].get.ts
@@ -0,0 +1,11 @@
+export default defineEventHandler(async (event) => {
+  const type = getRouterParam(event, 'type')
+  const id = getRouterParam(event, 'id')
+
+  switch(type){
+    case 'songs':      return await sendRedirect(event, `cider://play/s/${id}`);
+    case 'albums':     return await sendRedirect(event, `cider://play/a/${id}`);
+    case 'playlists':  return await sendRedirect(event, `cider://play/p/${id}`);
+  }
+  return `Uh Oh!\nCannot find the resource with type ${type} and id ${id}.\n\n\nPlease send a report to Discord(discord.gg/applemusic) - tag @yazninja if possible, thanks`
+});
