e527478bf19f20781e71f4056e5ef6548c125b3c
yazninja <yazninja@cider.sh>
2024-07-06 00:03:04 +0300

    add title template

--- app/pages/docs/[...slug].vue
+++ app/pages/docs/[...slug].vue
@@ -53,6 +53,9 @@
     title: computed(() => page.value?.title),
     description: computed(() => page.value?.description),
   });
+  useHead({
+    titleTemplate: "%s | Cider Docs",
+  });
 
   const { data: page, pending: pagePending } = await useAsyncData($route.path + "-data", async () =>
     queryContent($route.path).findOne()
