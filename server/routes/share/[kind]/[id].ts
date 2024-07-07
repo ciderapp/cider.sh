581c542233fc52f1e0a67b4240c61d372177641d
Core <core@coredev.uk>
2024-07-07 19:49:46 +0100

    Update [id].ts

--- server/routes/share/[kind]/[id].ts
+++ server/routes/share/[kind]/[id].ts
@@ -4,5 +4,6 @@ export default defineEventHandler(async (event) => {
 
   if (kind === "album") await sendRedirect(event, `https://album.link/i/${id}`);
   else if (kind === "song") await sendRedirect(event, `https://song.link/i/${id}`);
+  else if (kind === "podcast") await sendRedirect(event, `https://pods.link/i/${id}`);
   else await sendRedirect(event, `/404`);
 });
