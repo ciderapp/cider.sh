713aafcb75670b28493ad107173d6721825d0bf0
Monochromish <79590499+Monochromish@users.noreply.github.com>
2024-09-12 01:39:53 -0400

    Update the list and make it fancy

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,45 +1,61 @@
 <template>
-  <UiContainer class="pb-10">
-    <UiContainer class="py-16 text-center lg:py-24">
-      <slot name="headline">
-        <p v-if="headline" class="font-semibold text-primary">{{ headline }}</p>
-      </slot>
-      <slot name="title">
-        <h2 class="mb-4 mt-2 text-4xl font-bold lg:mb-6 lg:mt-3 lg:text-5xl">{{ title }}</h2>
-      </slot>
-      <slot name="description">
-        <p class="mx-auto max-w-[768px] text-lg text-muted-foreground lg:text-xl">
-          {{ description }}
-        </p>
-      </slot>
-      <div class="mt-5 flex w-full flex-col-reverse justify-center gap-3 lg:w-auto lg:flex-row">
-        <slot />
+  <main class="overflow-hidden">
+    <div
+      class="relative mx-auto flex h-max w-max items-center justify-center opacity-50 blur-[120px] filter"
+    >
+      <div class="absolute inset-0 flex items-center justify-center">
+        <div
+          class="circle absolute h-[40rem] w-[40rem] rounded-full"
+          :style="{ backgroundColor: circleColor }"
+        ></div>
       </div>
-    </UiContainer>
-    <section class="flex w-full flex-wrap items-center justify-center gap-x-5 gap-y-10">
-      <template v-for="member in members" :key="member.name">
-        <div class="flex w-44 flex-col items-center">
-          <UiAvatar
-            class="mb-5 h-24 w-24 shadow-2xl shadow-primary/20 transition-all hover:shadow-primary"
-            :src="member.avatar"
-          />
-          <p class="text-lg font-semibold">{{ member.name }}</p>
-          <p class="text-primary">{{ member.role }}</p>
-          <div class="mt-2 flex gap-2">
-            <NuxtLink :to="member.github" v-if="member.github">
-              <Icon name="mdi:github" class="h-6 w-6 transform-gpu hover:scale-110" />
-            </NuxtLink>
-            <NuxtLink :to="member.twitter" v-if="member.twitter">
-              <Icon name="mdi:twitter" class="h-6 w-6 transform-gpu hover:scale-110" />
-            </NuxtLink>
-            <NuxtLink :to="`mailto:${member.email}`" v-if="member.email">
-              <Icon name="mdi:email" class="h-6 w-6 transform-gpu hover:scale-110" />
-            </NuxtLink>
-          </div>
+    </div>
+    <UiContainer class="pb-10">
+      <UiContainer class="py-16 text-center lg:py-24">
+        <slot name="headline">
+          <p v-if="headline" class="font-semibold text-primary">{{ headline }}</p>
+        </slot>
+        <slot name="title">
+          <h2 class="mb-4 mt-2 text-4xl font-bold lg:mb-6 lg:mt-3 lg:text-5xl">{{ title }}</h2>
+        </slot>
+        <slot name="description">
+          <p class="mx-auto max-w-[768px] text-lg text-muted-foreground lg:text-xl">
+            {{ description }}
+          </p>
+        </slot>
+        <div class="mt-5 flex w-full flex-col-reverse justify-center gap-3 lg:w-auto lg:flex-row">
+          <slot />
         </div>
-      </template>
+      </UiContainer>
+      <section class="flex w-full flex-wrap items-center justify-center gap-x-5 gap-y-10">
+        <template v-for="member in members" :key="member.name">
+          <div class="flex w-44 flex-col items-center">
+            <UiAvatar
+              class="coloredShadow mb-5 h-24 w-24 transition-all"
+              :src="member.avatar"
+              @mouseenter="handleHover(member)"
+              @mouseleave="handleHover()"
+              :style="{
+                '--tw-shadow-color': member.color,
+              }"
+            />
+            <p class="text-lg font-bold">{{ member.name }}</p>
+            <p class="text-sm">{{ member.role.toUpperCase() }}</p>
+            <div class="mt-2 flex gap-2">
+              <NuxtLink :to="member.github" v-if="member.github">
+                <Icon name="mdi:github" class="h-6 w-6 transform-gpu hover:scale-110" />
+              </NuxtLink>
+              <NuxtLink :to="member.twitter" v-if="member.twitter">
+                <Icon name="mdi:twitter" class="h-6 w-6 transform-gpu hover:scale-110" />
+              </NuxtLink>
+              <NuxtLink :to="`mailto:${member.email}`" v-if="member.email">
+                <Icon name="mdi:email" class="h-6 w-6 transform-gpu hover:scale-110" />
+              </NuxtLink>
+            </div>
+          </div>
+        </template>
 
-      <!-- <template v-for="member in members" :key="member.name">
+        <!-- <template v-for="member in members" :key="member.name">
         <div class="flex w-44 flex-col items-center">
           <UiAvatar class="mb-5 h-24 w-24 ring-1 ring-ring/20" :src="member.avatar" />
           <p class="text-lg font-semibold">{{ member.name }}</p>
@@ -57,17 +73,29 @@
           </div>
         </div>
       </template> -->
-    </section>
-    <section class="pt-20">
-      <p class="mb-8 text-center text-lg text-muted-foreground">
-        We also want to give special thanks to our contributors in GitHub.
-      </p>
-      <NuxtLink to="https://github.com/ciderapp/cider/graphs/contributors" target="_blank">
-        <img src="https://contrib.rocks/image?repo=ciderapp/cider&columns=20" />
-      </NuxtLink>
-    </section>
-  </UiContainer>
+      </section>
+      <section class="pt-20">
+        <p class="mb-8 text-center text-lg text-muted-foreground">
+          We also want to give special thanks to our contributors in GitHub.
+        </p>
+        <NuxtLink to="https://github.com/ciderapp/cider/graphs/contributors" target="_blank">
+          <img src="https://contrib.rocks/image?repo=ciderapp/cider&columns=20" />
+        </NuxtLink>
+      </section>
+    </UiContainer>
+  </main>
 </template>
+
+<style scoped>
+  .circle {
+    transition: ease-in-out 0.2s;
+  }
+  .coloredShadow:hover {
+    box-shadow:
+      0 4px 6px -1px var(--tw-shadow-color),
+      0 10px 15px -3px var(--tw-shadow-color);
+  }
+</style>
 
 <script lang="ts" setup>
   const headline = "About Us";
@@ -84,6 +112,19 @@
     { text: "Company Three", icon: "logos:airbnb" },
     { text: "Company Four", icon: "logos:akamai" },
   ];
+
+  const circleColor = ref("#f32c56");
+
+  function handleHover(member: CollectiveMember | undefined) {
+    circleColor.value = member?.color ?? "#f32c56";
+  }
+
+  const rolePriority = {
+    Founder: 1,
+    "Lead Developer": 2,
+    Developer: 3,
+  };
+
   interface CollectiveMember {
     name: string;
     github?: string;
@@ -103,7 +144,7 @@
     {
       name: "cryptofyre",
       github: "https://github.com/cryptofyre",
-      role: "FOUNDER",
+      role: "Founder",
       roles: [
         "Founder",
         "Development Lead",
@@ -122,7 +163,7 @@
     {
       name: "Core",
       github: "https://github.com/coredev-uk",
-      role: "LEAD DEVELOPER",
+      role: "Lead Developer",
       roles: [],
       projects: ["ame", "cider-1", "cider-2"],
       avatar: "https://avatars.githubusercontent.com/u/64542347?v=4",
@@ -136,7 +177,7 @@
     {
       name: "Quacksire",
       github: "https://github.com/quacksire",
-      role: "DEVELOPER",
+      role: "Developer",
       roles: [],
       projects: ["ame", "cider-1", "cider-2", "connect"],
       avatar: "https://avatars.githubusercontent.com/u/19170969?v=4",
@@ -149,7 +190,7 @@
     {
       name: "booploops",
       github: "https://github.com/booploops",
-      role: "LEAD DEVELOPER",
+      role: "Lead Developer",
       roles: [],
       projects: ["ame", "cider-1", "cider-2"],
       avatar: "https://avatars.githubusercontent.com/u/49113086?v=4",
@@ -162,7 +203,7 @@
     {
       name: "vapormusic",
       github: "https://github.com/vapormusic",
-      role: "DEVELOPER",
+      role: "Developer",
       roles: [],
       projects: ["ame", "cider-1", "cider-2"],
       avatar: "https://avatars.githubusercontent.com/u/27716185?v=4",
@@ -173,7 +214,7 @@
     {
       name: "Maikiwi",
       github: "https://github.com/maikirakiwi",
-      role: "LEAD DEVELOPER",
+      role: "Lead Developer",
       roles: [],
       projects: ["cider-1", "cider-2"],
       avatar: "https://avatars.githubusercontent.com/u/74925636?v=4",
@@ -186,7 +227,7 @@
     {
       name: "yazninja",
       github: "https://github.com/yazninja",
-      role: "LEAD DEVELOPER",
+      role: "Lead Developer",
       roles: [],
       projects: ["cider-1", "cider-2"],
       avatar: "https://avatars.githubusercontent.com/u/71800112?v=4",
@@ -200,7 +241,7 @@
     {
       name: "GamingLiamStudios",
       github: "https://github.com/GamingLiamStudios",
-      role: "DEVELOPER",
+      role: "Developer",
       roles: [],
       projects: ["cider-1", "cider-2"],
       avatar: "https://avatars.githubusercontent.com/u/58615717?v=4",
@@ -213,15 +254,18 @@
     {
       name: "Amaru",
       github: "https://github.com/Amaru8",
-      role: "DEVELOPER",
+      role: "Developer",
       roles: [],
       projects: ["cider-1", "cider-2"],
       avatar: "https://avatars.githubusercontent.com/u/52407090?v=4",
+      email: "amaru@cider.sh",
+      color: "#007709",
+      songId: "1160370510",
     },
     {
       name: "lockieluke",
       github: "https://github.com/lockieluke",
-      role: "LEAD DEVELOPER",
+      role: "Lead Developer",
       roles: [],
       projects: ["cider-1", "cider-2", "cider-macos"],
       avatar: "https://avatars.githubusercontent.com/u/25424409?v=4",
@@ -231,22 +275,9 @@
       songTimestamp: 69,
     },
     {
-      name: "d3rpp",
-      github: "https://github.com/d3rpp",
-      role: "LEAD DEVELOPER",
-      roles: [],
-      projects: ["cider-2"],
-      avatar: "https://avatars.githubusercontent.com/u/45675641?v=4",
-      twitter: "https://twitter.com/d3rpp_dev",
-      email: "d3rpp@cider.sh",
-      quote: "look mum, I'm in the credits!",
-      color: "#212121",
-      songId: "1590796506",
-    },
-    {
       name: "Swiftzerr",
       github: "https://github.com/elliotjarnit",
-      role: "DEVELOPER",
+      role: "Developer",
       roles: [],
       projects: ["cider-2", "connect"],
       avatar: "https://avatars.githubusercontent.com/u/67812203?v=4",
@@ -257,21 +288,9 @@
       songId: "693388969",
     },
     {
-      name: "Numinous",
-      github: "https://github.com/StupeFied",
-      role: "DEVELOPER",
-      roles: [],
-      projects: ["cider-2"],
-      avatar: "https://avatars.githubusercontent.com/u/89881655?v=4",
-      twitter: "https://twitter.com/personwhoisran",
-      quote: `Like a sailor seeing the shore disappear, I watch my past recede, reduced to the ashes of memory.`,
-      color: "rgb(41, 181, 219)",
-      songId: "1591640885",
-    },
-    {
       name: "Monochromish",
       github: "https://github.com/Monochromish",
-      role: "DEVELOPER",
+      role: "Developer",
       roles: [],
       projects: ["cider-2"],
       avatar: "https://avatars.githubusercontent.com/u/79590499?v=4",
@@ -282,17 +301,21 @@
       songId: "1215409141",
     },
     {
-      name: "SoNothing",
-      github: "https://github.com/SoNothingMC",
-      role: "DEVELOPER",
+      name: "DeadFrost",
+      github: "https://github.com/DeadFrostt",
+      role: "Developer",
       roles: [],
       projects: ["cider-2"],
-      avatar: "https://avatars.githubusercontent.com/u/32032824?v=4",
-      twitter: "https://twitter.com/SoNotng",
-      email: "sonothing@cider.sh",
-      quote: `bigrat.monster`,
-      color: "#211657",
-      songId: "1704786170",
+      avatar: "https://avatars.githubusercontent.com/u/71704732?v=4",
+      twitter: "https://twitter.com/DeadFrostt",
+      quote: `Cider Janitor`,
+      color: "#FFB7CE",
+      songId: "157472443",
+      songTimestamp: 327,
     },
-  ];
+  ]
+    .slice()
+    .sort((a, b) => {
+      return rolePriority[a.role] - rolePriority[b.role];
+    });
 </script>
